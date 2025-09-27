4e703c0cfcf7a0dc088df27a328f3b7cadb83a68
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-27 16:08:06 +0200

    🎨 color the border

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -3,5 +3,9 @@ import type { FC } from 'react';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const HeroSection: FC<unknown> = () => {
-  return <section className={cn('border')}>Hero section</section>;
+  return (
+    <section className={cn('border', 'border-top-border')}>
+      Hero section
+    </section>
+  );
 };
