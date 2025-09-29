c92e1a424807932fcc856250acdc01a0c50f248f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-29 21:21:09 +0200

    📝 change og desc

--- app/constants/app-info.ts
+++ app/constants/app-info.ts
@@ -1,4 +1,5 @@
 export const APP_NAME = 'MotorPulse';
 export const APP_DEFAULT_TITLE = 'MotorPulse';
 export const APP_TITLE_TEMPLATE = `%s - ${APP_NAME}`;
-export const APP_DESCRIPTION = 'Generated from next-template';
+export const APP_DESCRIPTION =
+  'MotorPulse is tracker app that allows you to track things like mileage, car repairs etc.';
